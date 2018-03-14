860493c3291c4d5e0a080240c7a8fc3a9d42a4cb
Lila Colston-Nepali <12lmcn@queensu.ca>
2018-03-14 11:57:10 -0400

    download big data file from the dryad website

---
+++ downloadfile.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+wget https://datadryad.org/bitstream/handle/10255/dryad.71302/Ssalar_usat_genepop_23Sep2014.txt?sequence=1
+
+#This will allow me to download the dataset for Atlantic salmon microsats through the North American range
