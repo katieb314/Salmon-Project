feaac17365b281e07581e6b206ce63fedb6f5c3b
Lila Colston-Nepali <12lmcn@queensu.ca>
2018-03-15 09:57:09 -0400

    working on conversions

--- downloadfile.sh
+++ downloadfile.sh
@@ -2,3 +2,4 @@
 wget https://datadryad.org/bitstream/handle/10255/dryad.71302/Ssalar_usat_genepop_23Sep2014.txt?sequence=1
 
 #This will allow me to download the dataset for Atlantic salmon microsats through the North American range
+#Then I can use the command cp to move a copy to my  FileConversions folder. I will use mv to change the name to make it easier to execute further commands
