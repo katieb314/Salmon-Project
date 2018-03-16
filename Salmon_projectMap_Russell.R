8a7c43e82433dffe4e2b2463365365da590bed20
Russell <russturner23@gmail.com>
2018-03-16 09:54:15 -0400

    salmon project map

---
+++ Salmon_projectMap_Russell.R
@@ -0,0 +1,9 @@
+require(ggmap)
+PopRange <- c(min(LocaData$Longitude) ,
+              min(LocaData$Latitude ) ,
+              max(LocaData$Longitude) ,
+              max(LocaData$Latitude) )
+biomap <- ggmap(get_map(location=PopRange, source = "google", maptype = "satellite", zoom = 5))
+biomap+
+  geom_point(aes(x = Longitude, y= Latitude) , data= LocaData, color = "yellow", size = 2, alpha= 1)
+  
