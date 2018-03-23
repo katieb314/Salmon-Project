bacc169704c38d3ac8748dfc66b3410a9865911c
Russell <russturner23@gmail.com>
2018-03-23 15:37:08 -0400

    Changed the map to the correct data

--- Salmon_projectMap_Russell.R
+++ Salmon_projectMap_Russell.R
@@ -1,31 +1,20 @@
 require(ggmap)
 LocaData <- read.csv("mec12972-sup-0001-TableS1-S5.csv", header=T, sep=",",stringsAsFactors = T) #importing dataset from github
-brad2014 <- read.csv("Bradbury_2014_sites.csv")
-library(dplyr)
-LocaData2 <- brad2014 %>% mutate(longitude = Longitude*(-1), latitude =Latitude)
 
-# first dataset
+library(dplyr)
+
+# create the range for Google maps to make a basemap for our salmon sites
 PopRange <- c(min(LocaData$Longitude) ,
               min(LocaData$Latitude ) ,
               max(LocaData$Longitude) ,
               max(LocaData$Latitude) )
+# create the basemap and store it in r as a object using google's satellite
 biomap <- ggmap(get_map(location=PopRange, source = "google", maptype = "satellite", zoom = 5))
+# populate our map with our sampling sites
 biomap+
   geom_point(aes(x = Longitude, y= Latitude) , data= LocaData, color = "yellow", size = 2, alpha= 0.8)
-  geom_point(aes(x= Longitude, y= Latitude, data=LocaData2, color= "red", size=2, alpha=0.8))
-  
-  # second dataset
-  PopRange2 <- c(min(LocaData2$longitude) +2,
-                min(LocaData2$latitude ) +2,
-                max(LocaData2$longitude) +2,
-                max(LocaData2$latitude) +2)
-  
-  biomap <- ggmap(get_map(location=PopRange2, source = "google", maptype = "satellite", zoom = 5))
-  biomap+
-    geom_point(aes(x = longitude, y= latitude) , data= LocaData2, color = "red", size = 4, alpha= 1)+
-    geom_point(aes(x = Longitude, y= Latitude) , data= LocaData, color = "yellow", size = 1, alpha= 1)
 
-  
-  ??geocode
-geocode(c("jasper", "toronto") output=( "latlona"))
-geocodeQueryCheck()
+# try and add labells??
+  geom_text(aes(x = Longitude, y= Latitude), label=LocaData$Name, data= LocaData)
+ 
+            
