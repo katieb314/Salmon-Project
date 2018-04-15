d35c34cafb846e2a88b76c777025bd7eeb67f4cd
Stephanie Haddad <stephanie.haddad@queensu.ca>
2018-04-15 16:16:25 -0400

    added missing value to dataframe

--- results-[diffCalc]_BigFile/Meancalculationspw.R
+++ results-[diffCalc]_BigFile/Meancalculationspw.R
@@ -9,10 +9,11 @@ Df<-read_table2("results-[diffCalc]_BigFile/pairwise.txt",na = "empty", skip = 1
 summary(Df)#have to see what class it is, need numerics. 
 View(Df[8:159,]) #view to verify all values accounted for
 Data<-Df[8:159,]
-View(Data)
-pw<-sapply(Data, as.numeric, na.rm=TRUE)
+View(Data)####noticed that this contains all data except one value, last value 0.1805 
+Datas<-rbind(Data, c(0.1805)) #added that missing value
+pw<-sapply(Datas, as.numeric, na.rm=TRUE)
 View(pw)
 sum(pw, na.rm=TRUE)
-mean(pw, na.rm = TRUE)
+mean(pw, na.rm = TRUE) # 0.2483286
 range(pw, na.rm = TRUE) ##[1] -0.0248  0.7458 is it okay to have negative values ??
 
