3e09f40acd89af13bb6c61f260992ce7627ce0ab
Stephanie Haddad <stephanie.haddad@queensu.ca>
2018-04-19 00:50:58 -0400

    adding literture script and both pw gst together

---
+++ Litcit.R
@@ -0,0 +1,37 @@
+#"PMID":"10835412","PMCID":"PMC1461096" #Inference of population structure using multilocus genotype data.
+#"PMID":"12663554","PMCID":"PMC1462502" #Bayesian inference of recent migration rates using multilocus genotypes.
+#PMID: 10835412 PMCID: PMC1461096 #Inference of population structure using multilocus  genotpye data
+
+
+
+##Baillie, J. and B. Groombridge. 1996. “Salmo salar”. IUCN Red List of Threatened Species.  http://dx.doi.org/10.2305/IUCN.UK.1996.RLTS.T19855A9026693.en
+
+##doi 10.1111/mec.13395 Transatlantic secondary contact in Atlantic salmon paper
+##doi 10.1111/1755-0998.12157 NeEstimator V2: re-implementation of software for the estimation of contemporary effective population size (Ne) from genetic data
+
+## DOI 10.1002/joc.1276 Very high resolution interploted climate surfaces for global land ares
+
+#DOI  10.1111/2041-210X.12067 diversity: and R package for estimation of pop genetics
+##DOI 10.1093 PGD spider bioinformatics/article/28/2/298/198891
+
+# 10.1111/mec.12972. Conservation genomics  of anadromous Atlantic salmon
+
+
+dois = c('10.1111/2041-210X.12067','10.1093','')
+
+
+poster_litcited = function(dois=""){
+  write("",file = "literature_cited.txt")
+  for (i in dois){
+    ref = cr_cn(dois = i,format = "text", style = "journal-of-evolutionary-biology")
+    write(ref,file="literature_cited.txt",append = TRUE)
+  }
+  write("",file="poster_literature_cited.txt")
+  lit = read.delim("literature_cited.txt",sep="\t",fill=TRUE,header=FALSE,as.is=TRUE)
+  for (line in lit){
+    short_ref = gsub("(\\d\\d\\d\\d)(\\. .*)(\\. \\w*.*\\d+)(.*\\.).*\\.$","\\1\\3\\4", line)
+    write(short_ref,file="poster_literature_cited.txt",append = TRUE)
+  }
+}
+
+poster_litcited(dois)
--- resultsNewfFile-[diffCalc]/meancalcscript.R
+++ resultsNewfFile-[diffCalc]/meancalcscript.R
@@ -6,3 +6,12 @@ Gst<-sapply(Gst, as.numeric, na.rm=TRUE)
 sum(Gst, na.rm=TRUE) #598.2184
 mean(Gst, na.rm = TRUE)#0.2549951
 range(Gst, na.rm= TRUE)#0.0096 0.6257
+
+library(readr)
+pw <- read_table2("~/Salmon-Project/resultsREGIONSNewfFile-[diffCalc]/pairwise.txt",col_names = FALSE, na = "empty", skip = 8)
+View(pw)
+gst<-pw[20:31,2:13]
+gst<-sapply(gst, as.numeric, na.rm= TRUE)
+sum(gst, na.rm=TRUE) #12.0323
+mean(gst, na.rm = TRUE)#0.1542603
+range(gst, na.rm= TRUE)#0.0490 0.3166
