7647fd7e6598a9d99d8deb14c5bb756a4c80d354
Lila Colston-Nepali <12lmcn@queensu.ca>
2018-04-19 23:12:51 -0400

    moving things to folders

---
+++ diveRsityAnalyses/HWEBigFile-[diveRsity]_BigFile/loopHWE.R
@@ -0,0 +1,53 @@
+I= length(hwe[,2]) #runs through the length of column overall , numeric
+J= length(hwe[,1]) #runs through length column population names, character
+
+###if I is full length of overall column, we only want every 7th row selected
+###if J is full length pops, want every 9th row selected and would need to add the first pop name which is column 1 name?
+for (i in I){
+Store<-c()#create empty vector to store the numbers pulled from, these must be in order??? so
+
+for (i in I){
+  Store[i]<-(hwe[i])
+}
+print(Store)
+
+  Store[i]<-i
+}
+mean(Store) # once vector has numbers can find out
+range(Store)## have to store numbers first  
+
+print(c(i,j))
+
+while (i != 0.2993){
+  Store<-c(Store,i)
+  repeat
+}### for the end of the function of scanning overall values, it will repeat until finds 0.2993
+
+
+
+
+response<-#scan or grab function for overall values
+##end of loop....
+repeat {
+  print(response)
+  if (response == 0.2993){
+    break
+  }#2
+}#1
+
+
+
+
+
+
+
+##matrix making for loop..
+for (i in 1:nrow(x)) {
+  for (j in 1:ncol(x)) {
+    y[j,i] <- x[i,j] 
+  }
+}
+return(y)
+
+
+range 1:1375
